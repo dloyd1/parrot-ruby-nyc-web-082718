b7ba4b92eb80bac77a5dda16f596c3bb00995507
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 06:07:54 +0000

    Done.

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -11,3 +11,15 @@ describe '#parrot' do
     phrase = parrot
     expect(phrase).to eq("Squawk!")
   end
+
+  it 'should output the given phrase when called with an argument' do
+    expect($stdout).to receive(:puts).with("Pretty bird!")
+    parrot("Pretty bird!")
+  end
+
+  it 'should return the given phrase when called with an argument' do
+    phrase = parrot("Pretty bird!")
+
+    expect(phrase).to eq("Pretty bird!")
+  end
+end
